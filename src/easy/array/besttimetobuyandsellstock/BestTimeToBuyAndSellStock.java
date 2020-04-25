9b70227622324e59252d5c9ed2f7ce61dceab21b
john <369425422@qq.com>
2020-04-26 00:50:31 +0800

    提交题目

---
+++ src/easy/array/besttimetobuyandsellstock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,25 @@
+package easy.array.besttimetobuyandsellstock;
+
+/**
+ * BestTimeToBuyAndSellStock
+ *
+ * @author john 2020/4/25
+ */
+public class BestTimeToBuyAndSellStock {
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int max = 0;
+            if (prices.length > 0) {
+                int min = prices[0];
+                for (int i = 1; i < prices.length; i++) {
+                    if (prices[i] > min) {
+                        max = Math.max(max, prices[i] - min);
+                    } else {
+                        min = prices[i];
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}

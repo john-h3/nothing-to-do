f02596ce1be2a7d8fb5c5589655f6db86485f2a0
john <369425422@qq.com>
2020-08-07 16:01:08 +0800

    commit problem

---
+++ src/easy/array/shunshizhendayinjuzhenlcof/ShunShiZhenDaYinJuZhenLcof.java
@@ -0,0 +1,48 @@
+package easy.array.shunshizhendayinjuzhenlcof;
+
+/**
+ * ShunShiZhenDaYinJuZhenLcof
+ *
+ * @author john 2020/8/7
+ */
+public class ShunShiZhenDaYinJuZhenLcof {
+    class Solution {
+        public int[] spiralOrder(int[][] matrix) {
+            if (matrix.length == 0 || matrix[0].length == 0) {
+                return new int[0];
+            }
+            int row = matrix.length;
+            int col = matrix[0].length;
+            int[] answer = new int[row * col];
+            int top = 0, bottom = row - 1, left = 0, right = col - 1;
+            int idx = 0;
+            while (top <= bottom && left <= right) {
+                for (int c = left; c <= right; c++) {
+                    answer[idx++] = matrix[top][c];
+                }
+                for (int r = top + 1; r < bottom; r++) {
+                    answer[idx++] = matrix[r][right];
+                }
+                if (top != bottom) {
+                    for (int c = right; c >= left; c--) {
+                        answer[idx++] = matrix[bottom][c];
+                    }
+                }
+                if (left != right) {
+                    for (int r = bottom - 1; r > top; r--) {
+                        answer[idx++] = matrix[r][left];
+                    }
+                }
+                top++;
+                bottom--;
+                left++;
+                right--;
+            }
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        new ShunShiZhenDaYinJuZhenLcof().new Solution().spiralOrder(new int[][]{});
+    }
+}

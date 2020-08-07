acd49e27dde3c61f6811bfd948def633f5dfefe2
john <369425422@qq.com>
2020-08-07 10:19:16 +0800

    commit problem

---
+++ src/easy/array/countgoodtriplets/CountGoodTriplets.java
@@ -0,0 +1,26 @@
+package easy.array.countgoodtriplets;
+
+/**
+ * CountGoodTriplets
+ *
+ * @author john 2020/8/7
+ */
+public class CountGoodTriplets {
+    class Solution {
+        public int countGoodTriplets(int[] arr, int a, int b, int c) {
+            int count = 0;
+            for (int i = 0; i < arr.length - 2; i++) {
+                for (int j = i + 1; j < arr.length - 1; j++) {
+                    if (Math.abs(arr[i] - arr[j]) <= a) {
+                        for (int k = j + 1; k < arr.length; k++) {
+                            if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}

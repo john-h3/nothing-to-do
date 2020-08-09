a7838bdc02a12c4e1d0579154a86e1fcc0363f9f
john <369425422@qq.com>
2020-08-09 19:43:20 +0800

    commit problem

---
+++ src/easy/array/validpalindrome/ValidPalindrome.java
@@ -0,0 +1,42 @@
+package easy.array.validpalindrome;
+
+/**
+ * ValidPalindrome
+ *
+ * @author john 2020/8/9
+ */
+public class ValidPalindrome {
+    class Solution {
+        public boolean isPalindrome(String s) {
+            if (s.isEmpty()) {
+                return true;
+            }
+            int p1 = 0;
+            int p2 = s.length() - 1;
+            while (p1 < p2) {
+                char c1 = s.charAt(p1);
+                if (c1 >= 97 && c1 < 123) {
+                    c1 = (char) (c1 - 32);
+                }
+                if ((c1 >= 65 && c1 < 91) || (c1 >= 48 && c1 < 58)) {
+                    char c2 = s.charAt(p2);
+                    if (c2 >= 97 && c2 < 123) {
+                        c2 = (char) (c2 - 32);
+                    }
+                    if ((c2 >= 65 && c2 < 91) || (c2 >= 48 && c2 < 58)) {
+                        if (c1 != c2) {
+                            return false;
+                        }
+                        p1++;
+                        p2--;
+                    } else {
+                        p2--;
+                    }
+                } else {
+                    p1++;
+                }
+            }
+            return true;
+        }
+    }
+}
